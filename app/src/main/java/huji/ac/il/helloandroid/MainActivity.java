6e9446aae6977695d1138fcad60f46359114032e
yishai <yishai@glide.me>
2017-02-02 21:34:53 +0200

    send is now a button

--- app/src/main/java/huji/ac/il/helloandroid/MainActivity.java
+++ app/src/main/java/huji/ac/il/helloandroid/MainActivity.java
@@ -3,17 +3,19 @@ package huji.ac.il.helloandroid;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int MAX_NUMBER_OF_CHARS = 140;
     private static final String TAG = "MainActivity";
     EditText mTextInput;
     TextView mChatContent;
+    ImageView mSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,20 +23,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextInput = (EditText) findViewById(R.id.text_input);
-        mChatContent = (TextView) findViewById(R.id.conversation);
-
+        mSend = (ImageView) findViewById(R.id.send);
         mTextInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEND) {
-                    StringBuilder currentChat = new StringBuilder(mChatContent.getText());
-                    currentChat.append(v.getText()).append('\n');
-                    mChatContent.setText(currentChat.toString());
-                    mTextInput.setText("");
+                    appendLine(v);
                     return true;
                 }
                 return false;
             }
         });
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendLine(mTextInput);
+            }
+        });
+
+        mChatContent = (TextView) findViewById(R.id.conversation);
+
+
+    }
+
+    private void appendLine(TextView v) {
+        StringBuilder currentChat = new StringBuilder(mChatContent.getText());
+        currentChat.append(v.getText()).append('\n');
+        mChatContent.setText(currentChat.toString());
+        mTextInput.setText("");
     }
 }
